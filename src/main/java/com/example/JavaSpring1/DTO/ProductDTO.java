3639a465721a40c15a223ad86054d4d6b2c97a3a
Vũ Văn Quân <quancn27@gmail.com>
2026-01-09 22:57:19 +0700

    CHuẩn hoá lỗi spring boot

--- src/main/java/com/example/JavaSpring1/DTO/ProductDTO.java
+++ src/main/java/com/example/JavaSpring1/DTO/ProductDTO.java
@@ -1,20 +1,19 @@
 package com.example.JavaSpring1.DTO;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class ProductDTO {
+
     @NotBlank(message = "Tên sản phẩm không được để trống")
     private String name;
 
     @NotNull(message = "Giá sản phẩm không được để trống")
     @Positive(message = "Giá sản phẩm phải lớn hơn 0")
     private Double price;
-
+    @Size(max = 30)
+    @Positive(message = "Mô tả không được quá 30 kí tự")
     private String description;
 
     @NotNull(message = "Số lượng sản phẩm không được để trống")
